fc2ae510bbb431fda8caefbfc4b42a8465406e24
FelixSion <pisual@163.com>
2017-08-13 08:23:40 +0800

    Cache重构完毕 Part2

--- CacheResources/com/stargazerproject/cache/impl/resources/shell/OrderCahceShell.java
+++ CacheResources/com/stargazerproject/cache/impl/resources/shell/OrderCahceShell.java
@@ -1,10 +1,16 @@
-package com.stargazerproject.cache.impl.resources;
+package com.stargazerproject.cache.impl.resources.shell;
 
 import java.util.concurrent.ExecutionException;
+
+import javax.annotation.Resource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 
 import com.google.common.base.Optional;
 import com.google.common.cache.LoadingCache;
 import com.stargazerproject.cache.Cache;
+import com.stargazerproject.characteristic.BaseCharacteristic;
 import com.stargazerproject.model.order.impl.Order;
 
 /** 
@@ -18,13 +24,21 @@ import com.stargazerproject.model.order.impl.Order;
  *  @param <V> 缓存的Value类型
  *  @author Felixerio
  *  **/
-public class OrderCahceCharacteristic implements Cache<String, Order>{
+public class OrderCahceShell implements Cache<String, Order>, BaseCharacteristic<Cache<String, Order>>{
 
 	/** @illustrate 通用LoadingCache Guava 缓存接口 **/
+	@Resource(name="orderCacheLoadingCacheCharacteristic")
 	protected LoadingCache<String, Order> loadingCache;
 	
 	/** @construction 初始化构造 **/
-	public OrderCahceCharacteristic() {}
+	public OrderCahceShell() {}
+	
+	@Override
+	@Bean(name="orderCahceCharacteristicInitialize")
+	@Lazy(true)
+	public Optional<Cache<String, Order>> characteristic() {
+		return Optional.of(this);
+	}
 	
 	@Override
 	public void put(Optional<String> key, Optional<Order> value) {
