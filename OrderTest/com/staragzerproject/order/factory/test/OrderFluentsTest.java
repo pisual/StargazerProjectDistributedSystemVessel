64312a8a7240a34e143b6b01f664d7d3426d6b83
FelixSion <pisual@163.com>
2017-09-05 22:43:07 +0800

    UUID生成编写

--- OrderTest/com/staragzerproject/order/factory/test/OrderFluentsTest.java
+++ OrderTest/com/staragzerproject/order/factory/test/OrderFluentsTest.java
@@ -1,30 +1,51 @@
 package com.staragzerproject.order.factory.test;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
+import org.junit.Test;
+
+import com.fasterxml.uuid.Generators;
 import com.google.common.base.Optional;
 import com.staragzerproject.order.factory.OrderFluentFactory;
 import com.stargazerproject.cache.impl.OrderParameterCache;
 import com.stargazerproject.order.impl.Order;
 
 public class OrderFluentsTest {
-	public static void main(String[] args) {
+	
+	private static List<Order> orderList = new ArrayList<Order>();
+
+	private Order getOrder() {
 		OrderFluentFactory o = new OrderFluentFactory();
-  Order order = o.addID(Optional.of("ID"))
-		         .addTransmission().addReceiveTarget(Optional.of("192.168.1.1"), Optional.of(1010))
-		                           .addSourceTarget(Optional.of("192.168.1.2"), Optional.of(2020))
-		                           .next()
-		                           .addEvents()
-		                               .addCache(Optional.fromNullable(new OrderParameterCache()))
-		                               .addparameter(Optional.of("a"), Optional.of("a"))
-		                               .addnextEvent()
-		                           .addEvents()
-		                               .addCache(Optional.fromNullable(new OrderParameterCache()))
-                                       .addparameter(Optional.of("a"), Optional.of("a"))
-                              .complete();
-  
-  System.out.println(order.toString());
-  
+		UUID uuid = Generators.randomBasedGenerator().generate();
+		Order order = o.addID(Optional.of(uuid.toString())).addTransmission()
+				.addReceiveTarget(Optional.of("192.168.1.1"), Optional.of(1010))
+				.addSourceTarget(Optional.of("192.168.1.2"), Optional.of(2020)).next().addEvents()
+				.addCache(Optional.fromNullable(new OrderParameterCache()))
+				.addparameter(Optional.of("a"), Optional.of("a")).addnextEvent().addEvents()
+				.addCache(Optional.fromNullable(new OrderParameterCache()))
+				.addparameter(Optional.of("a"), Optional.of("a")).complete();
+		return order;
 	}
 	
+	private String getUUID(){
+		UUID uuid = Generators.randomBasedGenerator().generate();
+		System.out.println(uuid.toString());
+		return uuid.toString();
+	}
+
+	@Test
+	public void getSingleOrder() {
+		getOrder();
+	}
 	
+	@Test
+	public void getOrderList() {
+		for (int i = 0; i < 1000000; i++) {
+			orderList.add(getOrder());
+		}
+	}
+	
+
 }
