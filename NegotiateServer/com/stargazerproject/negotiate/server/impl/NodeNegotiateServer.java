ee3dfcea4efa1281231eb43f667e669a5f558770
FelixSion <pisual@163.com>
2018-01-03 23:11:04 +0800

    服务组件更新

--- NegotiateServer/com/stargazerproject/negotiate/server/impl/NodeNegotiateServer.java
+++ NegotiateServer/com/stargazerproject/negotiate/server/impl/NodeNegotiateServer.java
@@ -7,11 +7,11 @@ import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import com.google.common.base.Optional;
+import com.stargazerproject.interfaces.characteristic.shell.BaseCharacteristic;
 import com.stargazerproject.interfaces.characteristic.shell.StanderCharacteristicShell;
 import com.stargazerproject.negotiate.Negotiate;
 import com.stargazerproject.service.baseinterface.StanderServiceShell;
 import com.stargazerproject.service.util.ServiceUtil;
-import com.stargazerproject.spring.container.impl.BeanContainer;
 
 /** 
  *  @name nodenNegotiate服务的实现
@@ -25,19 +25,22 @@ public class NodeNegotiateServer implements StanderServiceShell{
 	
 	@Autowired
 	@Qualifier("nodenNegotiate")
-	private StanderCharacteristicShell<Negotiate> nodeNegotiateShell;
+	private StanderCharacteristicShell<Negotiate> nodeNegotiate;
+	
+	@Autowired
+	@Qualifier("nodenNegotiateShell")
+	private BaseCharacteristic<Negotiate> nodenNegotiateShell;
 	
 	/** @construction 初始化构造 **/
 	private NodeNegotiateServer() {}
 	
 	/** @illustrate 启动服务及相关操作 **/
 	@Override
-	@SuppressWarnings("unchecked")
 	public void startUp() {
 		ServiceUtil.dependOnDelay("systemParameterCacheServerListener", "localLogServerListener", "bigCacheIndexCacheServerListener");
-		Optional<Negotiate> nodeNegotiate = BeanContainer.instance().getBean(Optional.of("nodenNegotiateShell"), Optional.class);
-		nodeNegotiate.get().start();
-		nodeNegotiateShell.initialize(nodeNegotiate);
+		Optional<Negotiate> nodeNegotiateResources = nodenNegotiateShell.characteristic();
+		nodeNegotiateResources.get().start();
+		nodeNegotiate.initialize(nodeNegotiateResources);
 	}
 
 	/** @illustrate 关闭服务及相关操作 **/
